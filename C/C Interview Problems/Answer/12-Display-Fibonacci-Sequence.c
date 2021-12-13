2cb3c7111aa3fd0059a735d1f5ea2f4cd99400ab
Yourstruggle11 <sensouvik51@gmail.com>
2021-12-13 13:39:57 +0530

    [NEW] Added solution for problem no 12(C)

---
+++ C/C Interview Problems/Answer/12-Display-Fibonacci-Sequence.c
@@ -0,0 +1,22 @@
+# include<stdio.h>
+
+int main(){
+    int userInput, number1=0,number2=1,number3;
+
+    printf("Please enter a number:\n");
+    scanf("%d", &userInput);
+
+    printf("Fibonacci Sequence is :\n");
+    printf("%d %d ", number1,number2);
+    for (int i = 2; i < userInput; i++)
+    {
+        number3 = number1 + number2;
+        printf("%d ",number3);
+        number1 = number2;
+        number2 = number3;
+        ;
+    }
+    
+
+    return 0;
+}
